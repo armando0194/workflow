1f6beffa0f2779b7c4ab30bebd5a89d7068f5593
Armando <armando@Armandos-MacBook-Pro.local>
2019-06-29 13:46:56 -0700

    created ProgramingLanguage enum

---
+++ workflow/enums.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class ProgrammingLanguage(Enum): 
+    """ Enumerator class that represent the different 
+    programming languages that can be parsed
+    """
+    java = ".java"
+    python = ".py"
